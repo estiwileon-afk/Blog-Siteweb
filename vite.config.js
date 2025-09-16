52a0a28980d3fb11f508534e2ced5b1eb552146b
Estiff <estiwileon@gmail.com>
2025-09-16 15:18:53 -0500

    ojala ahora si

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         ensayos: resolve(__dirname, 'pages/Ensayos.html'),
         equipo: resolve(__dirname, 'pages/Nuestro Equipo.html'),
         ensayo1: resolve(__dirname, 'ensayos/1.html'),
-        ensayo1: resolve(__dirname, 'ensayos/2.html'),
+        ensayo2: resolve(__dirname, 'ensayos/2.html'),
       }
     }
   },
